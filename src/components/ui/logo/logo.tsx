137951a826f2400fda1f404b5715b9a9f4561659
aalend <aalendemirov@gmail.com>
2023-02-12 20:50:48 +0100

    fix: logo is changed to link and styles are changed also

--- src/components/ui/logo/logo.tsx
+++ src/components/ui/logo/logo.tsx
@@ -1,5 +1,12 @@
 import styles from './logo.module.scss';
+import { Link } from 'react-router-dom';
 
 export function Logo() {
-  return <div className={styles.logo}>myteam</div>;
+  return (
+    <div>
+      <Link to={'/'} className={styles.logo}>
+        myteam
+      </Link>
+    </div>
+  );
 }
