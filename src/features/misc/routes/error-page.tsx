dc09f16d5737ec779c6289295f8cc5f23356ade1
aalend <aalendemirov@gmail.com>
2023-02-26 19:38:02 +0100

    add error page

--- src/features/misc/routes/error-page.tsx
+++ src/features/misc/routes/error-page.tsx
@@ -1,3 +1,34 @@
+import { Link, useRouteError } from 'react-router-dom';
+
 export function ErrorPage() {
-  return <div>ErrorPage</div>;
+  const error = useRouteError();
+
+  return (
+    <div
+      id='error-page'
+      style={{
+        display: 'grid',
+        placeContent: 'center',
+        placeItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+      <Link
+        to={'/'}
+        style={{
+          marginTop: '32px',
+          color: 'var(--white-200)',
+          textDecoration: 'none',
+          fontSize: 'var(--step-1)',
+        }}
+      >
+        Back home
+      </Link>
+    </div>
+  );
 }
