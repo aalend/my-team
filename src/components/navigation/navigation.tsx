28952721cd932fa01b5cfc4c7ffa6b42389eb7c2
aalend <aalendemirov@gmail.com>
2023-02-26 19:21:51 +0100

    add mobile navbar

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,10 +1,41 @@
-import { NavLink } from 'react-router-dom';
-
+import { useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import styles from './navigation.module.scss';
 
-export function Navigation() {
+const Menu = function ({ onClose }: { onClose: () => void }) {
   return (
-    <nav className={styles.navigation}>
+    <>
+      <nav id='secondary' className={styles['mobile']}>
+        <ul>
+          <li>
+            <NavLink to={'/'}>home</NavLink>
+          </li>
+          <li>
+            <NavLink to={'/about'}>about</NavLink>
+          </li>
+          <li>
+            <NavLink to={'/contact'}>contact</NavLink>
+          </li>
+        </ul>
+        <button aria-label='Close menu' onClick={onClose}>
+          Close menu
+        </button>
+      </nav>
+    </>
+  );
+};
+
+export function Navigation() {
+  const [isOpen, setIsOpen] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    return () => setIsOpen(false);
+  }, [pathname]);
+
+  return (
+    <nav id='primary' className={styles.navigation}>
       <ul>
         <li>
           <NavLink to={'/'}>home</NavLink>
@@ -18,6 +49,18 @@ export function Navigation() {
           <NavLink to={'/contact'}>contact</NavLink>
         </li>
       </ul>
+      <button onClick={() => setIsOpen(!isOpen)} type='button' aria-label='open menu'>
+        Menu
+      </button>
+      {isOpen &&
+        createPortal(
+          <Menu
+            onClose={() => {
+              setIsOpen(false);
+            }}
+          />,
+          document.body
+        )}
     </nav>
   );
 }
